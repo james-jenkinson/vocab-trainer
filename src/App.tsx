60a1bdd3603ca5e1bee971f8bb5b9ccf26a76ef8
James Jenkinson <j.a.jenkinson@gmx.com>
2022-11-13 12:30:09 +0100

    Remove default styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
 import { A, Route, Routes } from '@solidjs/router';
 import Home from './views/Home';
 import Test from './views/Test';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <div>
+      <header>
         <A href='/'>Home</A>
         <A href='/test'>Test</A>
       </header>
@@ -29,7 +14,6 @@ const App: Component = () => {
         <Route path='/' element={Home} />
         <Route path='/test' element={Test} />
       </Routes>
-      test
     </div>
   );
 };
