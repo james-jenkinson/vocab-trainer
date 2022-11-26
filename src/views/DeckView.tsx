0447903ab2fb66d648658bd7ec8278f9296bc4e0
James Jenkinson <j.a.jenkinson@gmx.com>
2022-11-26 22:53:54 +0100

    Add due count

--- src/views/DeckView.tsx
+++ src/views/DeckView.tsx
@@ -1,4 +1,5 @@
 import { A, useParams } from '@solidjs/router'
+import { isBefore } from 'date-fns'
 import { JSX } from 'solid-js'
 import { db } from '../database/db'
 import { query } from '../database/query'
@@ -13,10 +14,21 @@ const DeckView = (): JSX.Element => {
     async () => await db.words.where('deckId').equals(deckId).count()
   )
 
+  const dueCount = query(
+    async () =>
+      await db.words
+        .where('deckId')
+        .equals(deckId)
+        .and((word) => isBefore(word.dueDate, new Date()))
+        .count()
+  )
+
   return (
     <main>
       <h1>{deck()?.name}</h1>
-      <h2>Word count: {wordCount()}</h2>
+      <h2>
+        Word count: {wordCount()} | Due: {dueCount()}
+      </h2>
       <div class="deck-actions">
         <A class="button" href={`/deck/${id}/add-word`}>
           Add word
